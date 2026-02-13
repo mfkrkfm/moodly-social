c04f6bb3cafac8ddb1833ac594a60b8c1b9c5ad3
mfkrkfm <igor.zatserkl@gmail.com>
2026-02-13 12:21:41 +0200

    initial commit

---
+++ moodly-social-api/src/test/java/com/example/moodly_social_api/MoodlySocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moodly_social_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodlySocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
