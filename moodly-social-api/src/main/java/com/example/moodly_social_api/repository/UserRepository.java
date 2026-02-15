daae6c7ca20b9b9f93952ddf53ce7272936cb62d
mfkrkfm <igor.zatserkl@gmail.com>
2026-02-15 18:44:50 +0200

    edit profile business logic implemented

--- moodly-social-api/src/main/java/com/example/moodly_social_api/repository/UserRepository.java
+++ moodly-social-api/src/main/java/com/example/moodly_social_api/repository/UserRepository.java
@@ -16,6 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
+    boolean existsByEmailAndIdNot(String email, Long currentUserId);
+
     @Transactional
     void deleteByUsername(String username);
 }
